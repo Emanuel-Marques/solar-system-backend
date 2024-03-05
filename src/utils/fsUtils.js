19b5ddfedbfb9bfa330ee9cc2ec17f0b93d8d3a8
Emanuel Marques <emanuelmarques585@gmail.com>
2024-03-05 02:18:11 +0100

    Adicionando dados

--- src/utils/fsUtils.js
+++ src/utils/fsUtils.js
@@ -1,9 +1,11 @@
 const fs = require('fs').promises;
 const path = require('path');
 
+const MISSION_DATA_PATH = '../../data/missions.json'; 
+
 async function readMissionsData() {
     try {
-        const data = await fs.readFile(path.resolve(__dirname,'../../data/missions.json'));
+        const data = await fs.readFile(path.resolve(__dirname, MISSION_DATA_PATH));
         const missions = JSON.parse(data);
         console.log(missions);
     } catch (error) {
@@ -12,4 +14,14 @@ async function readMissionsData() {
   
 }
 
-module.exports = { readMissionsData }
+async function writeNewMissionData(newMission) {
+    try {
+        const oldMissionData = await readMissionsData();
+        const newMissionData = [...oldMissionData, newMission];
+
+        await fs.writeFile(path.resolve(__dirname, MISSION_DATA_PATH));
+    } catch (e) {
+        throw new Error('Error writing new mission data', e.message);
+    }
+}
+module.exports = { readMissionsData, writeNewMissionData }
